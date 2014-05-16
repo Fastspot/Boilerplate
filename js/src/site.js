fb9a20659a3c1379d88aa2ea122680d586ee81d3
Ben Plum <mr@benplum.com>
2014-05-16 09:51:33 -0400

    Updating base scripts and styles

--- js/src/site.js
+++ js/src/site.js
@@ -1,45 +1,72 @@
-// Console Polyfill <http://stackoverflow.com/questions/3326650/console-is-undefined-error-for-internet-explorer>
-(function(){window.console||(window.console={});for(var b="log info warn error debug trace dir group groupCollapsed groupEnd time timeEnd profile profileEnd dirxml assert count markTimeline timeStamp clear".split(" "),a=0;a<b.length;a++)window.console[b[a]]||(window.console[b[a]]=function(){})})();
-// Fix Missing .indexOf()
-if(!Array.prototype.indexOf){Array.prototype.indexOf=function(e){var t=this.length>>>0;var n=Number(arguments[1])||0;n=n<0?Math.ceil(n):Math.floor(n);if(n<0)n+=t;for(;n<t;n++){if(n in this&&this[n]===e)return n}return-1}}
-// Fix Missing .forEach()
-if (!Array.prototype.forEach){Array.prototype.forEach=function(method,value){for(var i=0,c=this.length;i<c;i++){if(i in this){method.call(value,this[i],i,this);}}}};
-// Fix missing .trim()
-if(!String.prototype.trim){String.prototype.trim=function(){return this.replace(/^\s+|\s+$/g,'')}}
+/*-------------------------------------------
+  Site
+-------------------------------------------*/
 
 	// !IE
 	var IE8 = IE8 || false,
 		IE9 = IE9 || false;
 
 	// !Site
-	var Site = {
+	var Site = (function($, window) {
 		// Vars
-		minWidth: 320,
-		maxWidth: Infinity,
-		scrollTop: 0,
-		windowHeight: 0,
-		windowWidth: 0,
+		var minWidth = 320,
+			maxWidth = Infinity,
+			scrollTop = 0,
+			windowHeight = 0,
+			windowWidth = 0,
+			$window,
+			$doc,
+			$body,
+			$page,
+			$subnav;
 
-		_init: function() {
+		function init() {
 			// Objects
-			Site.$window = $(window);
-			Site.$doc    = $(document);
-			Site.$body   = $("body");
+			$window = $(window);
+			$doc    = $(document);
+			$body   = $("body");
+			$page   = $(".shifter-page");
+			$subnav = $(".subnavigation");
 
-			// Rubberband
-			Site.$window.on("snap", Site._onRespond)
-						.on("resize", Site._onResize)
-						.on("scroll", Site._onScroll);
+			// Init
+			$window.on("snap", onRespond)
+				   .on("resize", onResize);
+				   /* .on("scroll", onScroll); */
 
 			$.rubberband({
 				maxWidth: [ 1220, 980, 740, 500, 320 ],
 				minWidth: [ 1220, 980, 740, 500, 320 ]
 			});
+
+			$.shifter({
+				maxWidth: '979px'
+			});
+
 		}
-	};
+
+		function onRespond(e, data) {
+			maxWidth = data.maxWidth;
+			minWidth = data.minWidth;
+
+			$.shifter("close");
+		}
+
+		function onResize() {
+			//
+		}
+
+		function onScroll() {
+			//
+		}
+
+		return {
+			init: init
+		};
+	})(jQuery, window);
+
 
 	// !Ready
-	$(document).ready(Site._init);
+	$(document).ready(Site.init);
 
 
 
