fc60d7fce9f676d504ce4a3081df68476206ae40
Bryan Stoner <bryan@fastspot.com>
2017-05-24 12:15:57 -0400

    update lightbox control name

--- js/modules/page.js
+++ js/modules/page.js
@@ -32,7 +32,7 @@ Site.modules.Page = (function($, Site) {
 			theme: "fs-light",
 			viewer: false,
 			labels: {
-				close: "<span class='fs-lightbox-icon-previous'><svg class='symbol symbol_close'><use xmlns:xlink='http://www.w3.org/1999/xlink' xlink:href='#close'></use></svg></span>",
+				close: "<span class='fs-lightbox-icon-close'><svg class='symbol symbol_close'><use xmlns:xlink='http://www.w3.org/1999/xlink' xlink:href='#close'></use></svg></span>",
 				previous: "<span class='fs-lightbox-icon-previous'><svg class='symbol symbol_" + prev + "'><use xmlns:xlink='http://www.w3.org/1999/xlink' xlink:href='#" + prev + "'></use></svg></span>",
 				count: "<span class='fs-lightbox-meta-divider'></span>",
 				next: "<span class='fs-lightbox-icon-next'><svg class='symbol symbol_" + next + "'><use xmlns:xlink='http://www.w3.org/1999/xlink' xlink:href='" + next + "'></use></svg></span>"
