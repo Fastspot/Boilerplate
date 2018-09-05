951bb49f8d2d4be8dab67d44c361d6081b849e3f
Bryan Stoner <bryan@fastspot.com>
2018-09-05 12:13:44 -0400

    comb through testimonial markup

--- templates/callouts/testimonial.php
+++ templates/callouts/testimonial.php
@@ -10,37 +10,35 @@
 	 */
 ?>
 <div class="testimonial_block">
-	<div class="testimonial_block_inner">
-		<div class="fs-row">
-			<div class="fs-cell">
-				<div class="testimonial_item" itemscope itemtype="http://schema.org/<?=$site["schema_type"]?>">
+	<div class="fs-row">
+		<div class="fs-cell">
+			<div class="testimonial_block_inner">
+				<div class="testimonial" itemscope itemtype="http://schema.org/<?=$site["schema_type"]?>">
 					<meta itemprop="name" content="<?=$site["title"]?>">
-					<div class="testimonial_item_inner" itemprop="review" itemscope itemtype="http://schema.org/Review">
-						<div class="testimonial_item_media">
-							<figure class="testimonial_item_figure">
-								<img class="testimonial_item" src="<?=BigTree::prefixFile($image, "portrait-full-xsml-")?>" alt="">
+					<div class="testimonial_inner" itemprop="review" itemscope itemtype="http://schema.org/Review">
+						<div class="testimonial_media">
+							<figure class="testimonial_figure">
+								<img class="testimonial" src="<?=BigTree::prefixFile($image, "portrait-full-xsml-")?>" alt="">
 							</figure>
 						</div>
-						<div class="testimonial_item_wrapper">
-							<div class="testimonial_item_body">
-								<figure class="testimonial_item_quote">
-									<blockquote class="testimonial_item_quote_text" itemprop="reviewBody">
+						<div class="testimonial_wrapper">
+							<div class="testimonial_body">
+								<figure class="testimonial_quote">
+									<blockquote class="testimonial_quote_text" itemprop="reviewBody">
 										<p><?=nl2br($quote)?></p>
 									</blockquote>
-									<figcaption class="testimonial_item_quote_caption" itemprop="author" itemscope itemtype="http://schema.org/Person">
-										<span class="testimonial_item_quote_name" itemprop="name"><?=$name?></span> <?php if ($title) { ?> - <span class="testimonial_item_quote_title"><?=$title?></span><?php } ?>
+									<figcaption class="testimonial_quote_caption" itemprop="author" itemscope itemtype="http://schema.org/Person">
+										<span class="testimonial_quote_name" itemprop="name"><?=$name?></span> <?php if ($title) { ?> - <span class="testimonial_quote_title"><?=$title?></span><?php } ?>
 									</figcaption>
 								</figure>
 							</div>
-							<footer class="testimonial_item_footer">
+							<footer class="testimonial_footer">
 								<?php
 									if (!empty($link_title) && !empty($link_url)) {
 								?>
-								<div class="testimonial_item_links">
-									<a class="testimonial_item_link" <?=href($link_url)?> itemprop="url"<?php if (!empty($link_aria)) { ?> aria-label="<?=$link_aria?>"<?php } ?>>
-										<span class="testimonial_item_link_label"><?=$link_title?></span>
-									</a>
-								</div>
+								<a class="testimonial_link" <?=href($link_url)?> itemprop="url"<?php if (!empty($link_aria)) { ?> aria-label="<?=$link_aria?>"<?php } ?>>
+									<span class="testimonial_link_label"><?=$link_title?></span>
+								</a>
 								<?php
 									}
 								?>
