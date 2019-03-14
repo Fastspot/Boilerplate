efb22dd587cfbfbbfa2a0c45cd8a67cbfe15d8ca
Bryan Stoner <mimoduo@gmail.com>
2019-03-14 13:14:44 -0400

    sync news markup

--- templates/routed/news/default.php
+++ templates/routed/news/default.php
@@ -39,105 +39,103 @@
 
 <!-- insert filter drawing here -->
 
-<section class="news_listing">
-	<div class="news_listing_inner">
-		<div class="fs-row">
-			<div class="fs-cell">
-				<div class="news_listing_wrapper">
-					<div class="news_listing_body">
-						<ul class="news_list">
-							<?php
-								foreach ($news as $item) {
-							?>
-							<li class="news_item">
-								<article class="news_item_article" itemscope="itemscope" itemtype="http://schema.org/NewsArticle">
-									<?php
-										if (!empty($item["image"])) {
-									?>
-									<div class="news_item_media">
-										<a class="news_item_media_link" <?=href($item["link"])?>>
-											<figure class="news_item_figure">
-												<?php
-													include_with("picture.php", [
-														"class" => "news_item",
-														"image" => $item["image"],
-														"sources" => [
-															"740px" => "classic-sml-",
-															"0px" => "classic-xsml-"
-														]
-													]);
-												?>
-											</figure>
-										</a>
-									</div>
-									<?php
-										}
-									?>
-									<div class="news_item_wrapper">
-										<header class="news_item_header">
+<div class="news_block">
+	<div class="fs-row">
+		<div class="fs-cell">
+			<div class="news_block_inner">
+				<header class="news_header">
+					<h2 class="news_title">News</h2>
+				</header>
+				<div class="news_body">
+					<ul class="news_items" aria-label="News">
+						<?php
+							foreach ($news as $item) {
+						?>
+						<li class="news_item">
+							<article class="news_item_article" itemscope="itemscope" itemtype="http://schema.org/NewsArticle">
+								<?php
+									if (!empty($item["image"])) {
+								?>
+								<figure class="news_item_figure">
+									<a class="news_item_figure_link" <?=href($item["link"])?> tabindex="-1">
+										<?php
+											include_with("picture.php", [
+												"class" => "news_item",
+												"image" => $item["image"],
+												"sources" => [
+													"740px" => "classic-sml-",
+													"0px" => "classic-xsml-"
+												]
+											]);
+										?>
+									</a>
+								</figure>
+								<?php
+									}
+								?>
+								<div class="news_item_wrapper">
+									<header class="news_item_header">
+										<h3 class="news_item_title" itemprop="headline">
+											<a class="news_item_title_link" <?=href($item["link"])?>>
+												<span class="news_item_title_label"><?=$item["title"]?></span>
+											</a>
+										</h3>
+										<?php
+											if (is_array($item["categories"]) && count($item["categories"])) {
+										?>
+										<ul class="news_item_categories" aria-label="Categories">
 											<?php
-												if (is_array($item["categories"]) && count($item["categories"])) {
+												foreach ($item["categories"] as $category) {
 											?>
-											<div class="news_item_tags">
-												<?php
-													foreach ($item["categories"] as $category) {
-												?>
-												<div class="news_item_tag">
-													<a class="news_item_tag_link" href="<?=$page_link?>?category=<?=$category["route"]?>">
-														<span class="news_item_tag_label"><?=$category["title"]?></span>
-													</a>
-												</div>
-												<?php
-													}
-												?>
-											</div>
+											<li class="news_item_category">
+												<a class="news_item_category_link" href="<?=$page_link?>?category=<?=$category["route"]?>"><?=$category["title"]?></a>
+											</li>
 											<?php
 												}
 											?>
-											<h2 class="news_item_title" itemprop="headline">
-												<a class="news_item_title_link" <?=href($item["link"])?>><?=$item["title"]?></a>
-											</h2>
-										</header>
-										<div class="news_item_body">
-											<div class="news_item_meta">
-												<div class="news_item_date">
-													<time class="news_item_date_label" datetime="<?=$item["date"]?>" itemprop="datePublished"><?=date("F j, Y", strtotime($item["date"]))?></time>
-												</div>
-											</div>
-											<div class="news_item_description" itemprop="description">
-												<p><?=nl2br($item["blurb"])?></p>
+										</ul>
+										<?php
+											}
+										?>
+									</header>
+									<div class="news_item_body">
+										<div class="news_item_meta">
+											<div class="news_item_date">
+												<span class="news_item_date_label">Published: </span>
+												<time class="news_item_date_time" datetime="<?=$item["date"]?>" itemprop="datePublished"><?=date("F j, Y", strtotime($item["date"]))?></time>
 											</div>
 										</div>
-
-										<footer class="news_item_footer">
-											<div class="news_item_links">
-												<a class="news_item_link" <?=href($item["link"])?> aria-label="<?=$item["title"]?>" itemprop="url">
-													<span class="news_item_link_label">Read More</span>
-												</a>
-											</div>
-										</footer>
+										<div class="news_item_description" itemprop="description">
+											<p><?=nl2br($item["blurb"])?></p>
+										</div>
 									</div>
-								</article>
-							</li>
-							<?php
-								}
-							?>
-						</ul>
-					</div>
-					<footer class="news_listing_footer">
+									<footer class="news_item_links">
+										<a class="news_item_link" <?=href($item["link"])?> aria-label="<?=$item["title"]?>" itemprop="url" tabindex="-1">
+											<span class="news_item_link_label">Read More</span>
+											<span class="news_item_link_icon"><?=icon("chevron_right")?></span>
+										</a>
+									</footer>
+								</div>
+							</article>
+						</li>
 						<?php
-							if ($pages > 1) {
-								include_with("pagination.php", [
-									"pagination_link" => $pagination_link,
-									"pages" => $pages,
-									"current_page" => $current_page,
-									"max_to_show" => 10
-								]);
 							}
 						?>
-					</footer>
+					</ul>
 				</div>
+				<footer class="news_listing_footer">
+					<?php
+						if ($pages > 1) {
+							include_with("pagination.php", [
+								"pagination_link" => $pagination_link,
+								"pages" => $pages,
+								"current_page" => $current_page,
+								"max_to_show" => 10
+							]);
+						}
+					?>
+				</footer>
 			</div>
 		</div>
 	</div>
-</section>
+</div>
