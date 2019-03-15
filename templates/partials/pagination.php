01a180c304c536197a2c387e2fa9f96fe2a271c1
Bryan Stoner <mimoduo@gmail.com>
2019-03-15 13:56:21 -0400

    sync pagination

--- templates/partials/pagination.php
+++ templates/partials/pagination.php
@@ -12,40 +12,34 @@
 	$first_page = $pages_to_draw[0];
 	$last_page = $pages_to_draw[count($pages_to_draw) - 1];
 ?>
-<div class="pagination">
-	<a class="pagination_arrow pagination_arrow_left<?php if ($current_page == 1) { ?> pagination_arrow_disabled<?php } ?>" href="<?=$pagination_link.$prev_page.$hash?>">
-		<span class="pagination_arrow_icon"><?=icon("arrow_left")?></span>
-		<span class="pagination_arrow_label">Previous</span>
-	</a>
-	<nav class="pagination_nav">
-		<?php
-			if ($current_page != $first_page && $first_page != 1) {
-		?>
-		<span class="pagination_link pagination_more">&hellip;</span>
-		<?php
-			}
-
-			foreach ($pages_to_draw as $page_number) {
-				if ($page_number == $current_page) {
-		?>
-		<a href="<?=$pagination_link.$page_number.$hash?>" class="pagination_link active"><?=$page_number?></a>
-		<?php
-				} else {
-		?>
-		<a href="<?=$pagination_link.$page_number.$hash?>" class="pagination_link"><?=$page_number?></a>
-		<?php
-				}
-			}
-
-			if ($current_page != $last_page && $last_page != $pages) {
-		?>
-		<span class="pagination_link pagination_more">&hellip;</span>
-		<?php
-			}
-		?>
+<div class="js-pagination pagination">
+	<nav class="pagination_nav" aria-label="List">
+		<ul class="pagination_nav_list" aria-label="List">
+			<li class="pagination_nav_item">
+				<a class="js-pagination-previous pagination_arrow pagination_arrow_left<?php if ($current_page == 1) { ?> pagination_arrow_disabled<?php } ?>" href="<?=$pagination_link.$prev_page.$hash?>">
+					<span class="pagination_arrow_icon"><?=icon("arrow_left")?></span>
+					<span class="pagination_arrow_label">Previous</span>
+				</a>
+			</li>
+			<li class="pagination_nav_item">
+				<a class="js-pagination-next pagination_arrow pagination_arrow_right<?php if ($current_page == $pages) { ?> pagination_arrow_disabled<?php } ?>" href="<?=$pagination_link.$next_page.$hash?>">
+					<span class="pagination_arrow_label">Next</span>
+					<span class="pagination_arrow_icon"><?=icon("arrow_right")?></span>
+				</a>
+			</li>
+		</ul>
 	</nav>
-	<a class="pagination_arrow pagination_arrow_right<?php if ($current_page == $pages) { ?> pagination_arrow_disabled<?php } ?>" href="<?=$pagination_link.$next_page.$hash?>">
-		<span class="pagination_arrow_label">Next</span>
-		<span class="pagination_arrow_icon"><?=icon("arrow_right")?></span>
-	</a>
+	<form class="pagination_form" action="" method="get">
+		<div class="pagination_form_inner">
+			<label class="pagination_form_label" for="page">Page</label>
+			<div class="pagination_form_select_wrap">
+				<select class="js-pagination-select pagination_form_select" name="page" aria-label="Select Page Number">
+					<?php foreach ($pages_to_draw as $page_number) { ?>
+					<option value="<?=$page_number?>"><?=$page_number?></option>
+					<?php } ?>
+				</select>
+			</div>
+			<button class="pagination_form_button" type="submit" aria-label="View Page">Go</button>
+		</div>
+	</form>
 </div>
